307c969da9b185162f1e97e455a1bb00cc89ceff
kamber <kamber.sheshi10@gmail.com>
2024-09-21 15:52:32 +0200

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { data } from '../testapp-core/app';
 
 @Component({
   selector: 'app-root',
@@ -11,4 +12,5 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'testapp';
+  items = data;
 }
