25057641d195d2a3f781a18bd2466e7a33a40fb3
kamber <kamber.sheshi10@gmail.com>
2024-09-25 17:00:20 +0200

    test

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: {
+    enabled: true,
+    content: ['./src/**/*.{html,ts}'],
+  },
+  important: true,
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+// https://rafaelneto.dev/en/blog/integrate-tailwind-css-angular-material/
